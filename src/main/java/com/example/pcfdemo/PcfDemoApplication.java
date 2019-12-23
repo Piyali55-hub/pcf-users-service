dc7fa53bd2347dbcec8587a5e75c333d9c0d68d0
Piyali Das <official88piyali@gmail.com>
2019-12-23 01:32:17 -0500

    added service registry client

--- src/main/java/com/example/pcfdemo/PcfDemoApplication.java
+++ src/main/java/com/example/pcfdemo/PcfDemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.pcfdemo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PcfDemoApplication {
 
 	public static void main(String[] args) {
